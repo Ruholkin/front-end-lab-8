c9a77dcdba91934888ca0c718cd4a067203c5e8a
Olena Rukholla <ruholkin@gmail.com>
2018-02-17 15:12:49 +0200

    getCloserToZero_fix

--- homework_08/js/getClosestToZero.js
+++ homework_08/js/getClosestToZero.js
@@ -1,5 +1,5 @@
 function getClosestToZero () {
-	let variable;
+	let variable = Math.abs(arguments[0]);
 	for (let i = 0; i < arguments.length; i++){
 		if (Math.abs(arguments[i]) > Math.abs(arguments[i+1])) {
 			variable = Math.abs(arguments[i+1]);
