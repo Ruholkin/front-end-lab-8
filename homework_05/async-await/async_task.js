8d860e5233be8305e582d580b662dfe25fe847c4
Olena Rukholla <ruholkin@gmail.com>
2018-04-01 22:00:41 +0300

    add async_task

--- homework_05/async-await/async_task.js
+++ homework_05/async-await/async_task.js
@@ -29,11 +29,25 @@ async function doAsyncMagic () {
 doAsyncMagic(); // [true, true, true, true]
 
 async function* rangeGen() {
+
   for (let i = 1; i <= 15; i++) {
     yield i;
   }
 }
 
-/* Your code here */
+async function iterateRange () {
+  let sum = 0;
+  try {
+
+    for await( let i of rangeGen() ){
+      sum += i;
+    }
+    
+    console.log(sum);
+    return sum;
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 iterateRange(); // Promise {<resolved>: 120}
