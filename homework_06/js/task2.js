5d4ecc7230bfe1567f7fa8503541e98cc4bc7168
Olena Rukholla <ruholkin@gmail.com>
2018-04-03 23:22:33 +0300

    change checkIpAddress() in task2.js

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -107,7 +107,7 @@ function checkValue(value) {
 }
 
 function checkIpAddress(ipadress) {  
-  if ( /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipadress) 
+  if ( /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(ipadress.value) 
   	   || ipadress.value === "") {  
     return true;  
   }  
