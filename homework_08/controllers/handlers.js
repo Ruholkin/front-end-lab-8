ec22c0a3dc9d6805814a5ddb11dd7fa4a4dee698
Olena Rukholla <ruholkin@gmail.com>
2018-04-10 16:07:24 +0300

    add handlers.js

---
+++ homework_08/controllers/handlers.js
@@ -0,0 +1,164 @@
+const fs = require('fs');
+const path = require('path');
+const musiciansJson = path.join(__dirname, '..', 'data', 'storage.json');
+
+exports.get = (req, res) => {
+	fs.readFile( musiciansJson, (err, data) => {
+		let musicians;
+
+		if(err){
+			return res.sendStatus(500);
+		}
+
+		try{
+			musicians = JSON.parse(data);
+		} catch (e) {
+			return res.sendStatus(500);
+		}
+
+		res.status(200);
+		res.json(musicians);
+	});
+}
+
+exports.post = (req, res) => {
+	fs.readFile( musiciansJson, (err, data) => {
+		let musicians;
+		let musician;
+		let newMusician;
+
+		if(err){
+			return res.sendStatus(500);
+		}
+
+		try{
+			musicians = JSON.parse(data);
+		} catch (e) {
+			return res.sendStatus(500);
+		}
+
+		if(musician = musicians.find( el => el.name === req.body.name && el.band === req.body.band && el.instrument === req.body.instrument )){
+			return res.status(409).send({ "message" : "Musician already exist."});
+		}
+
+		newMusician = {
+			id: musicians.length + 1,
+			name: req.body.name,
+			band: req.body.band,
+			instrument: req.body.instrument
+		}
+		musicians.push(newMusician);
+
+		try{
+			fs.writeFile(musiciansJson, JSON.stringify(musicians), (err) => {
+        if (err) {
+          return res.sendStatus(500);
+        }
+    });
+		} catch (e) {
+			return res.sendStatus(500);
+		}
+		res.sendStatus(201);
+	});
+}
+
+exports.getById = (req, res) => {
+	fs.readFile( musiciansJson, (err, data) => {
+		let musicians;
+		let musician;
+
+		if(err){
+			return res.sendStatus(500);
+		}
+
+		try{
+			musicians = JSON.parse(data);
+		} catch (e) {
+			return res.sendStatus(500);
+		}
+
+		musician = musiciansJson.find( el => el.name === req.body.name && el.band === req.body.band && el.instrument === req.body.instrument);
+		if(musician){
+			return res.status(200).send(musician);
+		}else{
+			return res.sendStatus(404);
+		}
+	});
+}
+
+exports.put = (req, res) => {
+	fs.readFile( musiciansJson, (err, data) => {
+		let musicians;
+		let musician;
+
+		if(err){
+			return res.sendStatus(500);
+		}
+
+		try{
+			musicians = JSON.parse(data);
+		} catch (e) {
+			return res.sendStatus(500);
+		}
+
+		musician = musicians.find( el => el.id === parseInt(req.params.id) );
+
+		if(musician){
+			musician.name = req.body.name;
+			musician.band = req.body.band;
+			musician.instrument = req.bady.instrument;
+
+			musicians[ parseInt(req.params.id) - 1 ] = musician;
+
+			try{
+				fs.writeFile(musiciansJson, JSON.stringify(musicians), (err) => {
+        	if (err) {
+          	return res.sendStatus(500);
+        	}
+    		});
+			} catch (e) {
+				return res.sendStatus(500);
+			}
+			res.sendStatus(200);
+		}else{
+			return res.sendStatus(404);
+		}
+
+	});
+}
+
+exports.delete = (req, res) => {
+	fs.readFile( musiciansJson, (err, data) => {
+		let musicians;
+
+		if(err){
+			return res.sendStatus(500);
+		}
+
+		try{
+			musicians = JSON.parse(data);
+		} catch (e) {
+			return res.sendStatus(500);
+		}
+
+		musician = musicians.find( el => el.id === parseInt(req.params.id) );
+
+		if(musician){
+
+			musicians.splice( parseInt(req.params.id), 1 );
+
+			try{
+				fs.writeFile(musiciansJson, JSON.stringify(musicians), (err) => {
+        	if (err) {
+          	return res.sendStatus(500);
+        	}
+    		});
+			} catch (e) {
+				return res.sendStatus(500);
+			}
+			return res.status(200).send({ "message": "Musician has been successfully removed"});
+		}else{
+			return res.sendStatus(404);
+		}
+	});
+}
--- homework_08/routes.js
+++ homework_08/routes.js
@@ -1,17 +1,17 @@
 const express = require('express');
-const app = express.Router();
+const router = express.Router();
 const bodyParser = require('body-parser');
 const exportedFunctions = require('./controllers/handlers');
 
 router.use(bodyParser.urlencoded({
-	extended: true;
+	extended: true
 }));
 router.use(bodyParser.json());
 
-router.get("/musician", exportedFunctions.get);
-router.post("/musician", exportedFunctions.post);
-router.get("/musician/:id", exportedFunctions.getById);
-router.put("/musician/:id", exportedFunctions.put);
-router.delete("/musician/:id", exportedFunctions.delete);
+router.get("/rockstars", exportedFunctions.get);
+router.post("/rockstar", exportedFunctions.post);
+router.get("/rockstars/:id", exportedFunctions.getById);
+router.put("/rockstars/:id", exportedFunctions.put);
+router.delete("/rockstars/:id", exportedFunctions.delete);
 
 module.exports = router;
