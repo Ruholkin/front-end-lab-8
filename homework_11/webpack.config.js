5d31e3e420a10ded2cb513759870f9f37f874f45
Olena Rukholla <ruholkin@gmail.com>
2018-04-23 02:48:56 +0300

    almost done

--- homework_11/webpack.config.js
+++ homework_11/webpack.config.js
@@ -1,29 +1,37 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-	entry: "./src/js/interface-module.js",
-	plugins: [
+  entry: './src/js/interface-module.js',
+
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader'
+      })
+    },
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015']
+      }
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
     new HtmlWebpackPlugin({
-      title: "Homework 11",
-      template: "./src/index.html",
-      filename: "./dist/index.html"
-    })
-	],
-	// module: {
- //    rules: [{
- //      test: /\.css$/,
- //      loader: new ExtractTextPlugin.extract({
- //        use: {
- //          loader: "css-loader",
- //          options: {
- //            minimize: true
- //          }
- //        }
- //      })
- //  	}]
- //  },
-	output: {
-		filename: "./dist/bundle.js"
-	}
+      template: './src/index.html'
+  })
+],
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist')
+},
+
+  watch: true
 }
