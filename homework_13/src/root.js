8b7ac325582dedbf95f2bf6d8d90d8c92dc11854
Olena Rukholla <ruholkin@gmail.com>
2018-05-06 15:25:18 +0300

    add homework structure

---
+++ homework_13/src/root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Add index.scss file as dependency to handle with webpack loader
+import './index.scss';
+
+// import App from './app';
+import App from './features/app';
+
+// Append application to the DOM
+ReactDOM.render(
+  <App />, // The application container
+  document.querySelector("#root") // The container which hold the application
+);
